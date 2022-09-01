f4733f43bd1510de00be4284f98727cac67fc379
Joseph Lazaga <joseph.lazaga@oocl.com>
2022-09-01 15:02:01 +0800

    refactor: Refactored condition logic to make it shorter

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,23 +2,24 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String callNumber(int order) {
+        String message = "";
 
-        if (order % 3 == 0 && order % 5 == 0 && order % 7 == 0) {
-            return "FizzBuzzWhizz";
-        } else if (order % 3 == 0 && order % 5 == 0) {
-            return "FizzBuzz";
-        } else if (order % 3 == 0 && order % 7 == 0) {
-            return "FizzWhizz";
-        } else if (order % 5 == 0 && order % 7 == 0) {
-            return "BuzzWhizz";
-        } else if (order % 3 == 0) {
-            return "Fizz";
-        } else if (order % 5 == 0) {
-            return "Buzz";
-        } else if (order % 7 == 0) {
-            return "Whizz";
-        } else {
-            return String.valueOf(order);
+        if (order % 3 == 0) {
+            message = message + "Fizz";
         }
+
+        if (order % 5 == 0) {
+            message = message + "Buzz";
+        }
+
+        if (order % 7 == 0) {
+            message = message + "Whizz";
+        }
+
+        if (order % 3 != 0 && order % 7 != 0 && order % 5 != 0){
+            message = String.valueOf(order);
+        }
+
+        return message;
     }
 }
